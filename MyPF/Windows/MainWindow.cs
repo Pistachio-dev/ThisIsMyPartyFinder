34af1bbb21d4db0798c6a9aa9bf11a78f154ec14
Óscar Míguez Alonso <omiguezalonso@gmail.com>
2025-07-07 09:39:05 +0200

    Additions to main window.

--- MyPF/Windows/MainWindow.cs
+++ MyPF/Windows/MainWindow.cs
@@ -14,8 +14,9 @@ namespace SamplePlugin.Windows;
 
 public class MainWindow : Window, IDisposable
 {
-    private static readonly Vector4 LightGreen = new Vector4(156f / 255, 0, 143f / 255, 1);
-    private static readonly Vector4 DeepBlue = new Vector4(0, 0, 66 / 255, 1);
+    private static readonly Vector4 LightGreen = new Vector4(0, 199f / 255, 66f / 255, 1);
+    private static readonly Vector4 DeepBlue = new Vector4(0, 0, 150 / 255, 1);
+    private static readonly Vector4 Yellow = new Vector4(1, 1, 0, 1);
     private Plugin Plugin;
 
     // We give this window a hidden ID using ##
@@ -26,7 +27,7 @@ public class MainWindow : Window, IDisposable
     {
         SizeConstraints = new WindowSizeConstraints
         {
-            MinimumSize = new Vector2(375, 330),
+            MinimumSize = new Vector2(500, 430),
             MaximumSize = new Vector2(float.MaxValue, float.MaxValue)
         };
 
@@ -37,20 +38,21 @@ public class MainWindow : Window, IDisposable
 
     public override void Draw()
     {
+        ImGui.TextUnformatted("This plugin saves your PF listing, and replaces");
+        ImGui.SameLine();
+        ImGui.TextColored(LightGreen, "<mypf>");
+        ImGui.SameLine();
+        ImGui.TextUnformatted("with a link to it.");
+        ImGui.TextUnformatted("For instance:");
+        ImGui.TextColored(Yellow, "Blackjack open! <mypf> Password: 7777");
+        ImGui.TextUnformatted("Turns to:");
+        ImGui.TextColored(Yellow, "Blackjack open! Looking for Party (John Fantasyxiv) Password: 7777");
         SavedListingInfo? savedListing = ChatReader.GetSavedListing;
-
-        ImGui.TextColored(LightGreen, "Saved Party Finder listing:");
         ImGui.Separator();
-        using (var child = ImRaii.Child("Listing"))
-        {
-            if (child.Success)
-            {
-                DrawSavedListing(savedListing);
-            }            
-        }
-            
-
-        if (ImGuiComponents.IconButtonWithText(FontAwesomeIcon.Wrench, "Open settings", DeepBlue))
+        ImGui.TextColored(LightGreen, "Saved Party Finder listing:");        
+        DrawSavedListing(savedListing);
+        ImGui.Spacing();    
+        if (ImGuiComponents.IconButtonWithText(FontAwesomeIcon.Wrench, "Open settings"))
         {
             Plugin.ToggleConfigUI();
         }       
@@ -60,7 +62,7 @@ public class MainWindow : Window, IDisposable
     {
         if (savedListing == null)
         {
-            ImGui.TextUnformatted("None yet.");
+            ImGui.TextUnformatted("None yet. Reopen Party Finder to refresh.");
             return;
         }
 
